cb9197cb95612c4031057be778fdbf40cdf9738a
Sijan-code <110918527+Sijan-code@users.noreply.github.com>
2026-09-23 04:35:51 +0600

    Update main.py

--- main.py
+++ main.py
@@ -135,6 +135,11 @@ def extract_video_info(request: VideoRequest):
         'format': 'bestvideo+bestaudio/best',
         'no_warnings': True,
         'quiet': True,
+        'extractor_args': {
+             'youtube': {
+                'player_client': ['tv'],
+             }
+        },
         'http_headers': {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
